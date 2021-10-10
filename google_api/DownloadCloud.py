67e77bf9ac04ead6e63ad6953b8e077604161e9a
鄭翔安 <56338893+sean0922@users.noreply.github.com>
2021-10-11 00:55:23 +0800

    Add files via upload

---
+++ google_api/DownloadCloud.py
@@ -0,0 +1,10 @@
+import gdown
+
+url = "https://drive.google.com/u/0/uc?id=1e_ooUUWahVI0OyvritjITISOO-2AHoid&export=download"
+#output = "test.png"
+output = "1-1.jpg"
+gdown.download(url, output)
+#https://drive.google.com/file/d/1B-mcU9I_M2tkKUrgSSHdymCkWZgRgPTY/view?usp=sharing
+#https://drive.google.com/u/0/uc?id=1B-mcU9I_M2tkKUrgSSHdymCkWZgRgPTY&export=download    //1-1
+#https://drive.google.com/u/0/uc?id=1e_ooUUWahVI0OyvritjITISOO-2AHoid&export=download    //test
+#https://drive.google.com/file/d/1e_ooUUWahVI0OyvritjITISOO-2AHoid/view?usp=sharing
